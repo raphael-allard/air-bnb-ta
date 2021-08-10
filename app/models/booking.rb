bb92a3dcb6d34a13abb50cba669db491fcccd520
raphael allard <raphael.s.allard@gmail.com>
2021-08-10 11:25:13 +0200

    models validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :service
   has_many :reviews, dependent: :destroy
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,8 @@
 class Service < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :address, presence: true
 end
