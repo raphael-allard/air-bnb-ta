7f4c1fa3b2c9d1931925b64a00b98432f3d1112a
raphael allard <raphael.s.allard@gmail.com>
2021-08-11 10:36:06 +0200

    added create booking action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :services
+  resources :services do
+    resources :bookings, only: [:create]
+  end
 end
